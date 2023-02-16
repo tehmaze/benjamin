45c5068a4cc222d25a56f6828f1985a18ec588f5
maze <maze@pyth0n.org>
2023-02-16 21:51:28 +0100

    Doc

--- doc.go
+++ doc.go
@@ -1,2 +1,15 @@
-// Package benjamin contains drivers and user interface logic for interacting with button panels.
+// Package Benjamin, a Button panel UI
+//
+// This package contains drivers and user interface logic for interacting with
+// button panels, such as the popular Elgate Stream Decks.
+//
+// # Supported devices
+//
+// Devices supported by benjamin, are:
+//
+//	Elgato Stream Deck (original)
+//	Elgato Stream Deck V2 (original)
+//	Elgato Stream Deck Mini
+//	Elgato Stream Deck MK.2
+//	Elgato Stream Deck XL
 package benjamin
